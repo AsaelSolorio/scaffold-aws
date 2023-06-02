deb1f5b9c905e37d48af1db546c948e6f36ac817
EC2 Default User <ec2-user@ip-172-31-27-32.ec2.internal>
2023-06-02 01:53:00 +0000

    initial commmit

---
+++ hello.py
@@ -0,0 +1,7 @@
+# suma
+def suma(x: int, y: int) -> float:
+    return x + y
+
+
+result = suma(2.5, 6)
+print(f"this is the total: {result}")
---
+++ test_hello.py
@@ -0,0 +1,5 @@
+from hello import suma
+
+
+def test_file():
+    assert suma(2, 5) == 7
